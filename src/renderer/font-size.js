388d421115470fb4aa893123bbafbf6e27189c11
weizhenye <weizhenye@gmail.com>
2019-12-29 16:07:39 +0800

    fix: specify line height when get font size fix #21

--- src/renderer/font-size.js
+++ src/renderer/font-size.js
@@ -7,7 +7,7 @@ const cache = Object.create(null);
 export function getRealFontSize(fn, fs) {
   const key = `${fn}-${fs}`;
   if (!cache[key]) {
-    $fixFontSize.style.cssText = `font-size:${fs}px;font-family:"${fn}",Arial;`;
+    $fixFontSize.style.cssText = `line-height:normal;font-size:${fs}px;font-family:"${fn}",Arial;`;
     cache[key] = fs * fs / $fixFontSize.clientHeight;
   }
   return cache[key];
