c2c0ed5a7cc7513266cc3a49b595b9f92ce406e7
weizhenye <weizhenye@gmail.com>
2024-08-05 00:26:22 +0800

    fix: font-size in Firefox

--- src/renderer/font-size.js
+++ src/renderer/font-size.js
@@ -1,13 +1,16 @@
 // https://github.com/weizhenye/ASS/wiki/Font-Size-in-ASS
 
+// It seems max line-height is 1200px in Firefox.
+const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
+const unitsPerEm = isFirefox ? 512 : 2048;
+const lineSpacing = Object.create(null);
+
 export const $fixFontSize = document.createElement('div');
 $fixFontSize.className = 'ASS-fix-font-size';
+$fixFontSize.style.fontSize = `${unitsPerEm}px`;
 const $span = document.createElement('span');
 $span.textContent = '0';
 $fixFontSize.append($span);
-
-const unitsPerEm = 2048;
-const lineSpacing = Object.create(null);
 
 export function getRealFontSize(fn, fs) {
   if (!lineSpacing[fn]) {
