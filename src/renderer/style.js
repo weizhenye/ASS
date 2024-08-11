15a9ef87567d0bece680e6275fc3234f07ea45cc
weizhenye <weizhenye@gmail.com>
2024-08-11 17:24:19 +0800

    fix: use padding to match the width after transform

--- src/renderer/style.js
+++ src/renderer/style.js
@@ -3,14 +3,14 @@ export function createStyle(dialogue) {
   let cssText = '';
   if (layer) cssText += `z-index:${layer};`;
   cssText += `text-align:${['left', 'center', 'right'][align.h]};`;
-  if (!effect) {
+  if (!['banner', 'scroll up', 'scroll downn'].includes(effect?.name)) {
     cssText += `max-width:calc(100% - var(--ass-scale) * ${margin.left + margin.right}px);`;
     if (!pos) {
       if (align.h !== 0) {
-        cssText += `margin-right:calc(var(--ass-scale) * ${margin.right}px);`;
+        cssText += `padding-right:calc(var(--ass-scale) * ${margin.right}px);`;
       }
       if (align.h !== 2) {
-        cssText += `margin-left:calc(var(--ass-scale) * ${margin.left}px);`;
+        cssText += `padding-left:calc(var(--ass-scale) * ${margin.left}px);`;
       }
     }
   }
