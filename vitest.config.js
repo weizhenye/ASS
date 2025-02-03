3276d2a26ba2ab938ab215bb1b00421854ca0705
weizhenye <weizhenye@gmail.com>
2025-02-03 15:01:21 +0800

    chore: CI

--- vitest.config.js
+++ vitest.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     },
   ],
   test: {
-    include: 'test/test.js',
+    include: ['test/test.js'],
     browser: {
       enabled: true,
       provider: 'playwright',
